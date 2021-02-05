00794e5f6bc366752fc261b26bfe1924de07a366
Óscar Saá Pérez <osaaperez@danielcastelao.org>
2021-02-05 09:34:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Ejercicio de Squash con Hector
---
+++ src/com/squash/Oscar.java
@@ -0,0 +1,13 @@
+package com.squash;
+
+
+
+public class Oscar {
+
+      void metodoLider(){
+      System.out.println("squash");
+
+
+
+        }
+    }
